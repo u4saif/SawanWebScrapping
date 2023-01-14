7b80866f2b04be1c55a673bf645636571b73dfa8
eysaif <shaikh.saifuddin@in.ey.com>
2023-01-14 20:19:15 +0530

    song url created

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 const puppeteer = require('puppeteer');
 const fs = require("fs/promises");
 
+const delay = (milliseconds) => new Promise((resolve) => setTimeout(resolve, milliseconds));
+
 const {SONGS} = require('./constants');
 const songs=SONGS;
 
-console.log(songs)
-const delay = (milliseconds) => new Promise((resolve) => setTimeout(resolve, milliseconds));
+const songURL = songs.map((item)=>"https://saavn.me/songs?id="+item);
 
+let SaveSongURL= JSON.stringify(songURL);
+console.log(SaveSongURL);
+
+//*****************************//
+//******Write Data to DISK******
+function writeDataToDisk(fileName,dataToSave){
+    fs.writeFile(fileName,dataToSave,'utf8',function (err){
+        if (err) {
+            console.log("An error occured while writing JSON Object to File.");
+            return console.log(err);
+        }
+    });
+};
+/********************************/
+writeDataToDisk("SongURL",SaveSongURL);
 
 
 
