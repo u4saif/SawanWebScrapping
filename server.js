071332cadc76e2a0707bd445de5cda751f2784ae
eysaif <shaikh.saifuddin@in.ey.com>
2023-01-14 19:57:52 +0530

    constant created

---
+++ server.js
@@ -0,0 +1,24 @@
+const puppeteer = require('puppeteer');
+const fs = require("fs/promises");
+
+const {SONGS} = require('./constants');
+const songs=SONGS;
+
+console.log(songs)
+const delay = (milliseconds) => new Promise((resolve) => setTimeout(resolve, milliseconds));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
